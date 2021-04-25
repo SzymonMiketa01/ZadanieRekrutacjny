a308bfebd8b3083ca2bd735ee5d6ab3e9a3b6644
Szymon Miketa <szymon.miketa@comarch.pl>
2021-04-25 15:51:30 +0200

    Initial Commit

---
+++ ZadanieRekrutacyjnne/ZadanieRekrutacyjnneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZadanieRekrutacyjnneApp.swift
+//  ZadanieRekrutacyjnne
+//
+//  Created by Szymon Miketa on 25/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ZadanieRekrutacyjnneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
